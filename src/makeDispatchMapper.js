b9b14948641c58fbd555fefb5e5a74f959ca5ce0
Martin Engelking <martin.engelking@gmail.com>
2016-01-29 01:36:44 -0500

    Add optional 'key' arg to makeDispatchMapper. If 'key' is passed, then makeDispatchMapper will return an object with all the actions nested under that key (in another object).
    Normally the component sees actions mixed in `this.props` with Redux state props.
    e.g.
    this.props = { addTodo: function() {...}, todos: [...]}
    
    For larger applications it may be useful to gather all actions into a single key within this.props. If we pass 'actions' for the 'key' argument to makeDispatchMapper, our component will see:
    e.g.
    this.props = { actions: {addTodo: function() {...}}, todos: [...]}

--- src/makeDispatchMapper.js
+++ src/makeDispatchMapper.js
@@ -1,4 +1,4 @@
-module.exports = function (actionCreators) {
+module.exports = function (actionCreators, key) {
   return function (dispatch) {
     const props = {};
 
@@ -8,6 +8,13 @@ module.exports = function (actionCreators) {
       props[propName] = function (...args) { // eslint-disable-line
         return dispatch(actionCreator(...args));
       };
+    }
+
+    // If the optional 'key' argument was given, return an object which nests the props under that key.
+    if (typeof key == 'string' || typeof key == 'number') {
+      var nested_props = {};
+      nested_props[key] = props;
+      return nested_props;
     }
 
     return props;
